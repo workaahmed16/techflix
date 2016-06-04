a37f6bbb501a32a06900e4e8f116cdb391bd2b5f
Ahmed Ahmed <critenks29@gmail.com>
2016-06-04 22:12:21 +0000

    Listing out the courses

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   
+  resources :courses, only: [:index, :show]
+  
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
